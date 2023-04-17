bcfd3d4db673f22af9979de56530dcfda6b82452
Alt B 2022 <massie.w@gmail.com>
2023-04-16 17:25:24 -0700

    commented out require dotenv in connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-require('dotenv').config({ path: __dirname+"/../../.env" });
+// require('dotenv').config({ path: __dirname+"/../../.env" });
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/Book_Search_Engine', {
   useNewUrlParser: true,
